0f5edbd528526863aa172a877ce851e42b56252b
wlj <liangjun.wu@definesys.com>
2020-05-05 16:50:42 +0800

    设计模式-门面模式

--- spring-demo/design-mode/src/main/java/com/wlj/design/facade/app/Client.java
+++ spring-demo/design-mode/src/main/java/com/wlj/design/facade/app/Client.java
@@ -1,5 +1,7 @@
 package com.wlj.design.facade.app;
 
+import com.wlj.design.facade.Facade;
+
 /**
  * @Copyright: Shanghai Definesys Company.All rights reserved.
  * @Description:
@@ -8,4 +10,11 @@ package com.wlj.design.facade.app;
  * @history: 1.2020/5/5 created by liangjun.wu
  */
 public class Client {
+
+    public static void main(String[] args){
+        //高层模块，无需与具体的子系统交互
+        Facade facade = new Facade();
+        facade.methodA();
+        facade.methodB();
+    }
 }
